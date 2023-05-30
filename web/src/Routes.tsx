20c8349e190b0240450b9a429827020740552117
FX_SHADOW_KING <Nick@northroom.digital>
2023-05-30 15:56:23 +0200

    feat: fix

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,13 +1,17 @@
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
 
 const Routes = () => {
   return (
     <Router>
+      <Set prerender>
       <Route path="/assign-pothole" page={AssignPotholePage} name="assignPothole" />
       <Route path="/create-employee" page={CreateEmployeePage} name="createEmployee" />
       <Route path="/" page={LoginPage} name="login" />
       <Route path="/admin-dashboard" page={AdminDashboardPage} name="adminDashboard" />
       <Route notfound page={NotFoundPage} />
+
+      </Set>
+
     </Router>
   )
 }
